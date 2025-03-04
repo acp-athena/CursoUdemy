8963591f46b785014d6b320e0c1172c7d16a60d3
acp-athena <anacpaula83@gmail.com>
2025-03-04 11:35:21 -0300

    Exercicio finalizado

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -6,8 +6,6 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
-import javax.swing.JApplet;
-//asdsdasdsd
 import Entities.Department;
 import Entities.HourContract;
 import Entities.Worker;
